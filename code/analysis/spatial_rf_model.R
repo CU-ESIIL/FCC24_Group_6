710f28978502bb72d8e62d4623473e5f15064048
kysolvik <kysolvik@gmail.com>
2024-03-13 22:32:52 +0000

    Finalized RF model (non-spatial)

--- code/analysis/spatial_rf_model.R
+++ code/analysis/spatial_rf_model.R
@@ -1,40 +1,32 @@
-
+# Uncomment if needed to install packages
 # install.packages("spatialRF")
+# install.packages("Metrics")
+
+# Load required packages
 library(tidyverse)
 library(spatialRF)
-
+library(Metrics)
 
 # Set random seed
 random.seed <- 1
 
 # Read dataframe
 df <- read_csv('~/data/linked_disturbance_data_clean.csv')
-
 df <- df[!is.na(df['peakNDVI']),]
-
 # Set dependent and predictor variables
 dependent.variable.name <- "agbd"
 # predictor.variable.names <- colnames(df)[3:(length(colnames(df))-4)]
-predictor.variable.names <- c('chili', 'spei1YrPrior', 'spei5YrPrior',
-                              'spei10YrPrior', 'aetNorm', 'defNorm', 'forestCode',
+predictor.variable.names <- c('aetNorm', 'defNorm', 'forestCode',
                               'modisTreeVeg', 'peakNDVI', 'yrsSinceFire',
                               'yrsSinceInsect', 'yrsSinceHotDrought',
-                              'hex_id_5000', 'hex_id_10000', 'hex_id_50000',
-                              'hex_id_100000',
                               'utm_z13n_easting', 'utm_z13n_northing',
-                              'yrsSinceHotDrought..x..yrsSinceInsect', 
-                              'yrsSinceHotDrought..x..yrsSinceFire', 
+                              'yrsSinceHotDrought..x..yrsSinceInsect',
                               'yrsSinceFire..x..yrsSinceHotDrought', 
-                              'yrsSinceFire..x..yrsSinceInsect', 
-                              'yrsSinceInsect..x..yrsSinceHotDrought', 
-                              'yrsSinceInsect..x..yrsSinceFire')
+                              'yrsSinceFire..x..yrsSinceInsect')
 disturbance.variable.names <- c('yrsSinceFire','yrsSinceHotDrought','yrsSinceInsect',
-                                'yrsSinceHotDrought..x..yrsSinceInsect', 
-                                'yrsSinceHotDrought..x..yrsSinceFire', 
+                                'yrsSinceHotDrought..x..yrsSinceInsect',
                                 'yrsSinceFire..x..yrsSinceHotDrought', 
-                                'yrsSinceFire..x..yrsSinceInsect', 
-                                'yrsSinceInsect..x..yrsSinceHotDrought', 
-                                'yrsSinceInsect..x..yrsSinceFire')
+                                'yrsSinceFire..x..yrsSinceInsect')
 
 # Replace some NAs
 df$yrsSinceHotDrought <- df$yrsSinceHotDrought %>% replace_na(40)
@@ -48,28 +40,34 @@ for (var_name_i in var_list) {
   }
 }
 
-# X and y coordinates are utm easting/northing
-xy <- df[, c("utm_z13n_easting", "utm_z13n_northing")]
-colnames(xy) <- c('x','y')
 
-# euclidean_matrix <- dist(as.data.frame(xy)[1:100000,])
+train_df <- df[df$test_validation_training=='training',]
+val_df <-  df[df$test_validation_training=='validation',]
+
+# X and y coordinates are utm easting/northing
+train_xy <- train_df[, c("utm_z13n_easting", "utm_z13n_northing")]
+val_xy <- val_df[, c("utm_z13n_easting", "utm_z13n_northing")]
+
+colnames(train_xy) <- c('x','y')
+colnames(val_xy) <- c('x','y')
 
 # Fit basic model
 model.non.spatial <- spatialRF::rf(
-  data = df[1:20000,],
+  data = train_df,
   dependent.variable.name = dependent.variable.name,
   predictor.variable.names = predictor.variable.names,
-  xy = xy, #not needed by rf, but other functions read it from the model
+  xy = train_xy, #not needed by rf, but other functions read it from the model
   seed = random.seed,
   verbose = FALSE,
   ranger.arguments=list(
-    num.trees=500,
-    mtry=5,
-    min.node.size=10
+    num.trees=250,
+    mtry=3,
+    min.node.size=100
   )
 )
 model.non.spatial
 
+save(model.non.spatial, file='model2')
 
 # Plot residuals
 spatialRF::plot_residuals_diagnostics(
@@ -81,6 +79,29 @@ spatialRF::plot_residuals_diagnostics(
 spatialRF::plot_response_curves(
   model.non.spatial,
   variables=disturbance.variable.names,
-  quantiles = c(0.5),
+  quantiles = c(0.1, 0.5, 0.9),
   ncol = 3
 )
+
+# # Plot dependence surface
+# spatialRF::plot_response_surface(
+#   model.non.spatial,
+#   a='yrsSinceFire',
+#   b='yrsSinceHotDrought',
+# )
+
+spatialRF::plot_importance(
+  model.non.spatial,
+  verbose = FALSE
+  )
+
+# Inference
+predicted_model <- stats::predict(
+  object = model.non.spatial,
+  data = val_df,
+  type = "response"
+)
+
+# Write results
+val_df$predicted <- predicted_model$predictions
+write_csv(val_df, file='~/data/predicted_val.csv')
